852d1bfbdba075a0552ce0034e62bf7cf387c09b
Alex Parella <aaparella@ucdavis.edu>
2015-12-24 12:18:28 -0800

    Last time fixing documentation stuff

--- Error.go
+++ Error.go
@@ -4,7 +4,7 @@ import "github.com/gonum/matrix/mat64"
 
 type Error func([]float64, []float64) float64
 
-func ApplyFunc(fun func(int, int, float64) float64, mat *mat64.Dense) *mat64.Dense {
+func applyFunc(fun func(int, int, float64) float64, mat *mat64.Dense) *mat64.Dense {
 	res := mat64.DenseCopyOf(mat)
 	res.Apply(fun, res)
 	return res
